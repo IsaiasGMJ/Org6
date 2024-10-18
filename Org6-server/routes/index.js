568f9072f4486ce524da8bbb1a0d9d29024a7433
Isaias Martinez <Isaiasgmj@outlook.com>
2024-10-18 01:22:56 -0600

    Primeros cambios Mongo db controlador modelo y config db para auth

--- Org6-server/routes/index.js
+++ Org6-server/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var dotenv = require('dotenv');
+var connectDB = require('./config/db');
+var authRoutes = require('./routes/authRoutes');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+dotenv.config();
+//conectar a la base de datos
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+//rutas
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>{
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+})
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 module.exports = router;
